6c810e0dadb4f11b1a20c1713f6b7222d4d9d858
Abdul Mannan <mannansaudi@gmail.com>
2024-02-28 18:53:57 +0530

    Added validation to productController

--- backend/server/controllers/productController.js
+++ backend/server/controllers/productController.js
@@ -1,42 +1,83 @@
+import { body } from "express-validator";
 import { readImages } from "../middleware/imageUtils";
 import Product from "../models/productModel";
 import { PROD, TOP_PRODUCTS } from './../constants';
+import { checkValidation } from "../middleware/validation";
 
 // -------------------------------- Manage and view products --------------------------------
 
-export const addProduct = async (req, res, next) => {
-    try {
-        if (req.files.length)
-            req.body.images = readImages(req.files);
-        req.body.seller = req.user._id;
-        let newProduct = new Product(req.body);
-        await newProduct.save();
-        res.status(201).json(process.env.NODE_ENV === PROD
-            ? { message: "Product added successfully" }
-            : newProduct
-        );
-    } catch (err) {
-        next(err);
-    }
-};
+export const addProduct = [
 
-export const updateProduct = async (req, res, next) => {
-    try {
-        if (req.files.length)
-            req.body.images = readImages(req.files);
-        const product = await Product.findByIdAndUpdate(
-            req.params.productId,
-            req.body,
-            { new: true, runValidators: true }
-        ).select('-reviews -seller');
-        if (!product)
-            return res.status(404).json({ message: 'Product not found' });
+    body('name', 'Name must be specified.').trim().isLength({ min: 1 }).escape(),
+    body('description', 'Description must be specified.').trim().isLength({ min: 1 }).escape(),
+    body('category', 'Category must be specified.').trim()
+        .isIn(['Fertilizers', 'Pesticides', 'Seeds', 'Traps', 'Crop-Tonics']).escape(),
+    body('brand', 'Brand must be specified.').trim().isLength({ min: 1 }).escape(),
+    body('price', 'Price must be a positive number').trim().isFloat({ gt: 0 }),
+    body('quantity', 'Quantity must be a positive integer').trim().isInt({ gt: 0 }),
+    checkValidation(),
 
-        res.status(200).json(product);
-    } catch (err) {
-        next(err);
+    async (req, res, next) => {
+        try {
+            if (req.files.length)
+                req.body.images = readImages(req.files);
+            let newProduct = new Product({
+                name: req.body.name,
+                description: req.body.description,
+                category: req.body.category,
+                brand: req.body.brand,
+                price: req.body.price,
+                quantity: req.body.quantity,
+                seller: req.user._id,
+            });
+            await newProduct.save();
+            res.status(201).json(process.env.NODE_ENV === PROD
+                ? { message: "Product added successfully" }
+                : newProduct
+            );
+        } catch (err) {
+            next(err);
+        }
     }
-};
+]
+
+export const updateProduct = [
+
+    body('name', 'Name must be specified.').optional().trim().isLength({ min: 1 }).escape(),
+    body('description', 'Description must be specified.').optional().trim().isLength({ min: 1 }).escape(),
+    body('category', 'Category must be specified.').optional().trim()
+        .isIn(['Fertilizers', 'Pesticides', 'Seeds', 'Traps', 'Crop-Tonics']).escape(),
+    body('brand', 'Brand must be specified.').optional().trim().isLength({ min: 1 }).escape(),
+    body('price', 'Price must be a positive number').optional().trim().isFloat({ gt: 0 }),
+    body('quantity', 'Quantity must be a positive integer').optional().trim().isInt({ gt: 0 }),
+    checkValidation(),
+
+    async (req, res, next) => {
+        try {
+            if (req.files.length)
+                req.body.images = readImages(req.files);
+
+            const product = await Product.findByIdAndUpdate(
+                req.params.productId,
+                {
+                    name: req.body.name,
+                    description: req.body.description,
+                    category: req.body.category,
+                    brand: req.body.brand,
+                    price: req.body.price,
+                    quantity: req.body.quantity,
+                },
+                { new: true, runValidators: true }
+            ).select('-reviews -seller');
+            if (!product)
+                return res.status(404).json({ message: 'Product not found' });
+
+            res.status(200).json(product);
+        } catch (err) {
+            next(err);
+        }
+    }
+]
 
 export const deleteProduct = async (req, res, next) => {
     try {
@@ -124,46 +165,60 @@ export const getProductsBySeller = async (req, res, next) => {
 
 // -------------------------------- Reviews --------------------------------
 
-export const addReview = async (req, res, next) => {
-    try {
-        req.body.user = req.user._id;
-        const product = await Product.findByIdAndUpdate(
-            req.params.productId,
-            { $push: { reviews: req.body } },
-            { new: true, runValidators: true }
-        );
-        if (!product)
-            return res.status(404).json({ message: 'Product not found' });
+export const addReview = [
 
-        res.status(200).json(product.reviews[product.reviews.length - 1]);
-    } catch (err) {
-        next(err);
+    body('rating', 'Rating must be a number between 1 and 5').isInt({ min: 1, max: 5 }),
+    body('description', 'Description must be a string').optional().isString().trim().escape(),
+    checkValidation(),
+    
+    async (req, res, next) => {
+        try {
+            req.body.user = req.user._id;
+            const product = await Product.findByIdAndUpdate(
+                req.params.productId,
+                { $push: { reviews: req.body } },
+                { new: true, runValidators: true }
+            );
+            if (!product)
+                return res.status(404).json({ message: 'Product not found' });
+    
+            res.status(200).json(product.reviews[product.reviews.length - 1]);
+        } catch (err) {
+            next(err);
+        }
     }
-};
+]
 
-export const updateReview = async (req, res, next) => {
-    try {
-        const product = await Product.findOneAndUpdate(
-            { _id: req.params.productId, "reviews.user": req.user._id },
-            {
-                $set: {
-                    "reviews.$.rating": req.body.rating,
-                    "reviews.$.description": req.body.description,
+export const updateReview = [
+    
+    body('rating', 'Rating must be a number between 1 and 5').optional().isInt({ min: 1, max: 5 }),
+    body('description', 'Description must be a string').optional().isString().trim().escape(),
+    checkValidation(),
+
+    async (req, res, next) => {
+        try {
+            const product = await Product.findOneAndUpdate(
+                { _id: req.params.productId, "reviews.user": req.user._id },
+                {
+                    $set: {
+                        "reviews.$.rating": req.body.rating,
+                        "reviews.$.description": req.body.description,
+                    },
                 },
-            },
-            { new: true, runValidators: true }
-        );
-        if (!product)
-            return res.status(404).json({ message: 'Product not found' });
-
-        const review = product.reviews.find(review =>
-            review.user.toString() === req.user._id
-        );
-        res.status(200).json(review);
-    } catch (err) {
-        next(err);
+                { new: true, runValidators: true }
+            );
+            if (!product)
+                return res.status(404).json({ message: 'Product not found' });
+    
+            const review = product.reviews.find(review =>
+                review.user.toString() === req.user._id
+            );
+            res.status(200).json(review);
+        } catch (err) {
+            next(err);
+        }
     }
-};
+] 
 
 export const deleteReview = async (req, res, next) => {
     try {
